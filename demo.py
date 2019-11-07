7226588df930e9cc5e2afb3db508617ba5a64bc5
Tom Viner <tom.viner@sohonet.com>
2019-11-07 20:31:42 +0000

    thrust working

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
+import math
+
 alien = Actor('rocket2')
 alien.pos = 100, 56
-alien.velocity = -2, -3
+alien.velocity = [0, 0]
 
 WIDTH = 800
 HEIGHT = 800
@@ -14,11 +16,20 @@ def update():
     alien.left += alien.velocity[0]
     alien.top += alien.velocity[1]
     if alien.left > WIDTH:
-        alien.right = 0
+        alien.left = 0
     if alien.top > HEIGHT:
-        alien.bottom = 0
+        alien.top  = 0
     if alien.left < 0:
-        alien.right = WIDTH
+        alien.left = WIDTH
     if alien.top < 0:
-        alien.bottom = HEIGHT
+        alien.top = HEIGHT
+
+def on_key_down(key, mod, unicode):
+    if key == keys.LEFT:
+        alien.angle += 20
+    elif key == keys.RIGHT:
+        alien.angle -= 20
+    elif key == keys.SPACE:
+        alien.velocity[0] -= math.sin(math.radians(alien.angle))
+        alien.velocity[1] -= math.cos(math.radians(alien.angle))
 
